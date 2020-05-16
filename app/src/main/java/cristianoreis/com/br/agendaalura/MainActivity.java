2f42a34103e7972e81d20bcefcc2370b1b795560
Cristiano Reis <cristianoreisnascimento@gmail.com>
2020-05-15 23:07:37 -0300

    Class 3

--- app/src/main/java/cristianoreis/com/br/agendaalura/MainActivity.java
+++ app/src/main/java/cristianoreis/com/br/agendaalura/MainActivity.java
@@ -2,6 +2,8 @@ package cristianoreis.com.br.agendaalura;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,11 +19,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José"));
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
